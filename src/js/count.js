e58c4e6af222913a5631ed1474ce766d6b38e94a
Mathilda Segerlund <ms228qs@student.lnu.se>
2025-09-03 14:41:15 +0200

    Update countVowelsConsonants function to check if array of vowels includes char from input name

--- src/js/count.js
+++ src/js/count.js
@@ -9,11 +9,14 @@ export function countVowelsConsonants (name) {
   let vowels = 0
   let consonants = 0
 
+  let arrOfVowels = ['a', 'e', 'i', 'o', 'u', 'å', 'ä', 'ö']
+
   for (let i = 0; i < name.length; i++) {
-    if (i === a, e, i, o, u, å, ä, ö) {
+    if (arrOfVowels.includes(name[i])) {
       vowels += 1 
     } else {
       consonants += 1
     }
   }
+  return {vowels, consonants}
 }
