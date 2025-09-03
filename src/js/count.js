3d36ee85f1d45b4aaf3dfef0ad777ba03fa3af2f
Mathilda Segerlund <ms228qs@student.lnu.se>
2025-09-03 13:48:30 +0200

    Create and export function countVowelsConsonants

---
+++ src/js/count.js
@@ -0,0 +1,10 @@
+/**
+ * A function to count vowels and consonants in a word
+ *
+ * @author Mathilda Segerlund <ms228qs@student.lnu.se>
+ * @version 1.0.0
+ */
+
+export function countVowelsConsonants () {
+    
+}
---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+/**
+ * The main script file of the application.
+ *
+ * @author Mathilda Segerlund <ms228qs@student.lnu.se>
+ * @version 1.0.0
+ */
