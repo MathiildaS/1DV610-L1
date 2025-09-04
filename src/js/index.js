4bb66015cbb18589dfc5d1af288d2612df0c4623
Mathilda Segerlund <ms228qs@student.lnu.se>
2025-09-04 13:34:57 +0200

    Create function getCatImage and fetch random cat image from thecatapi. Create image element and replace previous image with new

--- src/js/index.js
+++ src/js/index.js
@@ -20,9 +20,33 @@ consonants.textContent = countedName.consonants
 
 document.getElementById("userName").textContent = enteredName.value;
 document.getElementById("greeting").style.display = "block";
+getCatImage()
 })
 
 document.addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
     enterBtn.click()
 }})
+
+async function getCatImage() {
+  const url = "https://api.thecatapi.com/v1/images/search"
+  try {
+    const response = await fetch(url)
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`)
+    }
+
+    const result = await response.json()
+    console.log(result)
+
+    const image = document.createElement('img')
+    image.src = `${result[0].url}`;
+    image.alt = "A random cat"
+    image.style.maxWidth = "300px"
+    image.style.borderRadius = "15px"
+
+    document.getElementById('catImage').replaceChildren(image)
+  } catch (error) {
+    console.error(error.message)
+  }
+}
